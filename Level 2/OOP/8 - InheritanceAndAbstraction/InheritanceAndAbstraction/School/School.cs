50bff14225718d59f57a11ad49eadd5691f79262
rvm <rvmladenov@gmail.com>
2014-10-05 21:24:37 +0300

    partial implementation for Level 2 -> OOP -> Problem 8

---
+++ Level 2/OOP/8 - InheritanceAndAbstraction/InheritanceAndAbstraction/School/School.cs
@@ -0,0 +1,15 @@
+﻿namespace InheritanceAndAbstraction.School
+{
+    using System;
+    using System.Collections.Generic;
+
+    abstract class School
+    {
+        private IList<SchoolClass> classes { get; set; }
+
+        public School(IList<SchoolClass> classes)
+        {
+            this.classes = classes;
+        }
+    }
+}
---
+++ Level 2/OOP/8 - InheritanceAndAbstraction/InheritanceAndAbstraction/School/SchoolClass.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace InheritanceAndAbstraction.School
+{
+    class SchoolClass
+    {
+    }
+}
